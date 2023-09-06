ad1021349449436742ea0fabc173928eec84389f
Sébastien Guimmara <sebastien.guimmara@oslandia.com>
2023-09-06 08:44:49 +0200

    style(OverlayPanel): format

--- src/components/panels/OverlayPanel.vue
+++ src/components/panels/OverlayPanel.vue
@@ -11,34 +11,35 @@ const overlays = Overlays.getOverlays()
 
 <template>
   <div class="py-2">
-    <h6>Basemaps</h6>
-    <ul id="basemap-list" class="layers-list-group">
-      <BasemapItem
-        v-for="layer in basemaps"
-        :key="layer.name"
-        :opacity="layer.opacity"
-        :name="layer.name"
-        :isLoading="layer.isLoading"
-        :visible="layer.visible"
-        :hasOpacitySlider="layer.type === 'color'"
-        v-on:update:visible="() => { layer.visible = !layer.visible; $forceUpdate() }"
-        v-on:update:opacity="(o) => { layer.opacity = o; $forceUpdate() }"
-      />
-    </ul>
+    <div id="basemap-list">
+      <h6>Basemaps</h6>
+      <ul class="layers-list-group">
+        <BasemapItem v-for="layer in basemaps"
+          :key="layer.name"
+          :opacity="layer.opacity"
+          :name="layer.name"
+          :isLoading="layer.isLoading"
+          :visible="layer.visible"
+          :hasOpacitySlider="layer.type === 'color'"
+          v-on:update:visible="() => { layer.visible = !layer.visible; $forceUpdate() }"
+          v-on:update:opacity="(o) => { layer.opacity = o; $forceUpdate() }" />
+      </ul>
+    </div>
     <hr>
-    <h6>Overlays</h6>
-    <ul id="overlay-list" class="layers-list-group">
-      <OverlayItem
-        v-for="item in overlays"
-        :key="item.name"
-        :name="item.name"
-        :visible="item.visible"
-        :opacity="item.opacity"
-        v-on:update:visible="() => { item.visible = !item.visible; $forceUpdate() }"
-        v-on:update:opacity="(o) => { item.opacity = o; $forceUpdate() }"
-        v-on:update:move-up="() => { item.moveUp(); $forceUpdate() }"
-        v-on:update:move-down="() => { item.moveDown(); $forceUpdate() }"
-      />
-    </ul>
+    <div id="overlay-list">
+
+      <h6>Overlays</h6>
+      <ul class="layers-list-group">
+        <OverlayItem v-for="item in overlays"
+          :key="item.name"
+          :opacity="item.opacity"
+          :name="item.name"
+          :visible="item.visible"
+          v-on:update:visible="() => { item.visible = !item.visible; $forceUpdate() }"
+          v-on:update:opacity="(o) => { item.opacity = o; $forceUpdate() }"
+          v-on:update:move-up="() => { item.moveUp(); $forceUpdate() }"
+          v-on:update:move-down="() => { item.moveDown(); $forceUpdate() }" />
+      </ul>
+    </div>
   </div>
 </template>
