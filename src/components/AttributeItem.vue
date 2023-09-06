a4dd0b09401da77f5555a826ce65ca957a937609
Sébastien Guimmara <sebastien.guimmara@oslandia.com>
2023-09-06 10:17:23 +0200

    fix(AttributeItem): avoid stringification of objects

--- src/components/AttributeItem.vue
+++ src/components/AttributeItem.vue
@@ -3,12 +3,24 @@ defineProps<{
     attrName: string,
     attrValue: any
 }>()
+
+function formatValue(item: any) {
+    if (typeof item === 'object') {
+        if (!Array.isArray(item)) {
+            // Any self-reference or circular reference in the object cannot be reliably stringified,
+            // thus we cannot properly display an arbitrary object as attribute value.
+            return 'object';
+        }
+    }
+
+    return item;
+}
 </script>
 
 <template>
     <tr>
         <td :title="attrName"><b>{{ attrName.substring(0, 18) }}</b></td>
-        <td :title="attrValue">{{ attrValue }}</td>
+        <td :title="attrValue">{{ formatValue(attrValue) }}</td>
     </tr>
 </template>
 
