a904965c81ae4682838ef7932377721eeee6a24c
Sébastien Guimmara <sebastien.guimmara@oslandia.com>
2023-09-08 12:11:52 +0200

    cleanup

--- src/giro3d/PointCloudMaterial.ts
+++ src/giro3d/PointCloudMaterial.ts
@@ -1,11 +1,13 @@
 import { Color, Uniform } from 'three';
-import * as chroma from 'chroma-js'
+import chroma from 'chroma-js'
 import PointsMaterial from '@giro3d/giro3d/renderer/PointsMaterial.js';
 
 import PointsVS from './PointsVS.glsl.js';
 import PointsFS from './PointsFS.glsl.js';
 
 class PointCloudMaterial extends PointsMaterial {
+    colorMapChanged: boolean;
+
     constructor(options = {}) {
         super(options);
         this.vertexShader = PointsVS;
