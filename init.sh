6e96d1e4d2e27ce2c76577198e71e3420f9db1b8
Thomas Muguet <thomas.muguet@oslandia.com>
2024-04-05 12:24:10 +0200

    fix(init.sh): don't overwrite existing config

--- init.sh
+++ init.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 npm --silent install
-cp ./src/config.ts.sample ./src/config.ts
-cp ./src/styles.ts.sample ./src/styles.ts
+cp -n ./src/config.ts.sample ./src/config.ts
+cp -n ./src/styles.ts.sample ./src/styles.ts
 
 echo Configuration generated. Start the server with \'npm run start\'.
