39b9e77f809aa35882d6b0062488163d2f3cea94
Sébastien Guimmara <sebastien.guimmara@oslandia.com>
2025-06-11 13:27:16 +0200

    build: temporarily copy the main.ts from avalon

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,32 @@
-import './assets/main.scss';
-
 import { createPinia } from 'pinia';
 import { createApp } from 'vue';
+
+import EntityPanel from '@giro3d/giro3d/gui/EntityPanel';
+
+import './assets/main.scss';
+
 import App from './App.vue';
+import { registerLoader } from './loaders/loader';
+import { registerEntityBuilder } from './giro3d/EntityBuilder';
 import { loadConfig } from './config-loader';
 
-async function start(): Promise<void> {
-    await loadConfig();
+import { buildEntity } from '../../app/lithojson/LithoJSONEntity';
+import LithoJSONEntityInspector from '../../app/lithojson/LithoJSONEntityInspector';
+import { importFromFile } from '../../app/lithojson/importer';
 
-    const app = createApp(App);
-    app.use(createPinia());
-    app.mount('#app');
+async function start(): Promise<void> {
+  // @ts-expect-error typing issue
+  EntityPanel.registerInspector('LithoJSONEntity', LithoJSONEntityInspector);
+
+  // @ts-expect-error typing issue
+  registerLoader('litho.json', importFromFile);
+  registerEntityBuilder('lithojson', buildEntity);
+
+  await loadConfig();
+
+  const app = createApp(App);
+  app.use(createPinia());
+  app.mount('#app');
 }
 
-start();
+start().catch(console.error);
