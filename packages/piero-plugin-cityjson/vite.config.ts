bbe52a7c0f187a0bc6793a804ed7f741e1855b54
Sébastien Guimmara <sebastien.guimmara@oslandia.com>
2025-09-26 10:27:59 +0200

    build(vite): fix incorrect bundling of piero-plugin-cityjson
    The bundle was incorrect because:
    - node_modules were not correctly identified as external by rollup.
    This is fixed by using the rollup-plugin-node-externals plugin.
    - @giro3d/giro3d was not added as a peer dependencies, leading Giro3D
    to be bundled, since the plugin did not detect it as an external (it
    relies on dependencies found in package.json)
    - no need to inline workers since they now work correctly in the demo
    app
    - no need to have special optimize configuration for
    cityjson-threejs-loader

--- packages/piero-plugin-cityjson/vite.config.ts
+++ packages/piero-plugin-cityjson/vite.config.ts
@@ -1,12 +1,9 @@
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import child_process from 'child_process';
 import { fileURLToPath, URL } from 'node:url';
-import path from 'path';
 import { nodeExternals } from 'rollup-plugin-node-externals';
 import { defineConfig } from 'vite';
 import dts from 'vite-plugin-dts';
-
-import inlineWorker from '../../build/vite-plugin-inline-worker';
 
 let commitHash = 'unknown';
 try {
@@ -23,7 +20,6 @@ const config = defineConfig(e => {
     const mode = e.mode;
     const isProduction = mode === 'production';
     const root = __dirname + '/';
-    const modules = path.resolve(root, '../../node_modules');
 
     return {
         build: {
@@ -45,19 +41,9 @@ const config = defineConfig(e => {
 
             sourcemap: isProduction,
         },
-        optimizeDeps: {
-            // We have an issue with the cityjson-three-loader which can be resolved by not optimizing it
-            // however it depends on earcut which _has_ to be optimized (because giro3d also depends on it)
-            exclude: ['cityjson-threejs-loader'],
-            include: ['earcut'],
-        },
         plugins: [
-            nodeExternals({
-                // We bundle the cityjson library directly since it makes uses of workers.
-                exclude: ['cityjson-threejs-loader'],
-            }),
+            nodeExternals(),
             nodeResolve(),
-            inlineWorker(),
             dts({
                 insertTypesEntry: true, // generates an entrypoint .d.ts
             }),
@@ -65,10 +51,6 @@ const config = defineConfig(e => {
         resolve: {
             alias: {
                 '@': fileURLToPath(new URL('./src', import.meta.url)),
-                // Use our dependencies for openbim-components & stuff
-                three: path.resolve(modules, 'three'),
-                // Use our dependencies for @math.gl
-                proj4: path.resolve(modules, 'proj4'),
             },
         },
         root,
